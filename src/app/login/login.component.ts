247b8390dad992c0bcaa4b4110a25e22f5187358
AngeloRP93 <angelorp93@gmail.com>
2019-05-12 20:26:23 -0500

    Login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  private inicializarFormulario() {
+  private inicializarFormulario() { 
     this.login = this._formBuilder.group( {
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
