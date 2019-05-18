2d87465fa8cbe30b672493c7099c1e10cd3577c1
artmadeit <this.arthur.email@gmail.com>
2019-05-18 14:12:29 -0500

    chore: add prettier for format

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,47 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  login: FormGroup
-  hide = true
-  constructor ( private _formBuilder: FormBuilder ) {
-    this.inicializarFormulario()
+  login: FormGroup;
+  hide = true;
+  constructor(private _formBuilder: FormBuilder) {
+    this.inicializarFormulario();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   private inicializarFormulario() {
-    this.login = this._formBuilder.group( {
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    } )
+    this.login = this._formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required]
+    });
   }
 
   emailValido(): boolean {
-    return this.login.get( 'email' ).valid
+    return this.login.get("email").valid;
   }
 
   emailDirty(): boolean {
-    return this.login.get( 'email' ).dirty
+    return this.login.get("email").dirty;
   }
 
   contrasenaValida(): boolean {
-    return this.login.get( 'password' ).valid
+    return this.login.get("password").valid;
   }
 
   getErrorMessage(): string {
-    return 'Este campo es requerido'
+    return "Este campo es requerido";
   }
 
   onSubmit() {
-    if ( this.login.valid ) {
-      console.log('Ingreso');
+    if (this.login.valid) {
+      console.log("Ingreso");
 
       // this.loading = true
       // this.payLoad = JSON.stringify( this.login.value )
@@ -60,5 +59,4 @@ export class LoginComponent implements OnInit {
       //   } )
     }
   }
-
 }
