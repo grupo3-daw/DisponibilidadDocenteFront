f48b17e531e4330c2f7e9856e13d37c7588e9d24
AngeloRP93 <angelorp93@gmail.com>
2019-05-23 23:19:18 -0500

    Redireccion a Pantalla Profesor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { Formulario } from '@shared/formulario/formulario';
-
+import {Router} from '@angular/router'
 import { LoginService } from './login.service';
 
 @Component({
@@ -12,7 +12,7 @@ import { LoginService } from './login.service';
 export class LoginComponent extends Formulario {
   hide = true;
 
-  constructor(private _loginService: LoginService) {
+  constructor(private _loginService: LoginService,private router:Router) {
     super([
       { name: 'id', validators: [Validators.required] },
       { name: 'password', validators: [Validators.required] }
@@ -21,6 +21,7 @@ export class LoginComponent extends Formulario {
 
   onSubmit(): void {
     if (this.formGroup.valid) {
+      this.router.navigate(['profesor']);
       // console.log("Ingreso");
       // this.loading = true
       // this.payLoad = JSON.stringify( this.login.value )
