e77d8b468d26f90720177a18556fef6011d6bcdf
AngeloRP93 <angelorp93@gmail.com>
2019-05-23 23:20:13 -0500

    Acomodando toolbar y cambiando vista de disponibilidad docente rol profesor

--- src/app/negocio/profesor/disponibilidad-semanal/disponibilidad-semanal.component.ts
+++ src/app/negocio/profesor/disponibilidad-semanal/disponibilidad-semanal.component.ts
@@ -1,4 +1,14 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+
+export type DiaLaborable =
+  | 'lunes'
+  | 'martes'
+  | 'miercoles'
+  | 'jueves'
+  | 'viernes'
+  | 'sabado';
 
 export class SemanaLaborable {
   hora: string;
@@ -20,6 +30,15 @@ export class SemanaLaborable {
   }
 }
 
+export class SeleccionSemana {
+  lunes = new SelectionModel<SemanaLaborable>(true, []);
+  martes = new SelectionModel<SemanaLaborable>(true, []);
+  miercoles = new SelectionModel<SemanaLaborable>(true, []);
+  jueves = new SelectionModel<SemanaLaborable>(true, []);
+  viernes = new SelectionModel<SemanaLaborable>(true, []);
+  sabado = new SelectionModel<SemanaLaborable>(true, []);
+}
+
 @Component({
   selector: 'app-disponibilidad-semanal',
   templateUrl: './disponibilidad-semanal.component.html',
@@ -36,7 +55,7 @@ export class DisponibilidadSemanalComponent implements OnInit {
     'viernes',
     'sabado'
   ];
-  dataSource: Array<SemanaLaborable> = [
+  dataSource = new MatTableDataSource<SemanaLaborable>([
     new SemanaLaborable('8:00-9:00'),
     new SemanaLaborable('9:00-10:00'),
     new SemanaLaborable('10:00-11:00'),
@@ -51,7 +70,53 @@ export class DisponibilidadSemanalComponent implements OnInit {
     new SemanaLaborable('19:00-20:00'),
     new SemanaLaborable('20:00-21:00'),
     new SemanaLaborable('21:00-22:00')
-  ];
+  ]);
+  selection = new SeleccionSemana();
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle(dia: DiaLaborable): void {
+    for (let index = 0; index < this.dataSource.data.length; index++) {
+      const resultado = this.actualizarDisponibilidad(dia, index);
+      console.log(this.horas);
+      if (resultado === 0) {
+        index = this.dataSource.data.length;
+      }
+    }
+  }
+
+  isAllSelected(dia: DiaLaborable): boolean {
+    let seleccionados = 0;
+    this.dataSource.data.map(row => {
+      if (row[dia]) {
+        seleccionados++;
+      }
+    });
+
+    return seleccionados === this.dataSource.data.length;
+  }
+
+  tieneElementosSeleccionados(dia: DiaLaborable): boolean {
+    return this.dataSource.data.findIndex(row => row[dia]) > -1;
+  }
+
+  actualizarDisponibilidad(dia: DiaLaborable, numeroFila: number): number {
+    if (this.dataSource.data[numeroFila][dia]) {
+      this.horas++;
+      console.log('Desmarcando Celda');
+      this.dataSource.data[numeroFila][dia] = false;
+    } else {
+      if (this.horas > 0) {
+        console.log('Marcando Celda');
+        this.horas--;
+        this.dataSource.data[numeroFila][dia] = true;
+      } else {
+        return 0;
+      }
+    }
+
+    return 1;
+  }
+
   constructor() {}
 
   ngOnInit() {}
