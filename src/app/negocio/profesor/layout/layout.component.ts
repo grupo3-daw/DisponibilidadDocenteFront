78f972d13f33d022c4042a9542baf2a8aa00f325
AngeloRP <angelorp93@gmail.com>
2019-05-30 23:32:37 -0500

    Cambios en el formato layout de vista profesor

--- src/app/negocio/profesor/layout/layout.component.ts
+++ src/app/negocio/profesor/layout/layout.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component( {
+@Component({
   selector: 'app-layout-profesor',
   templateUrl: './layout.component.html',
-  styleUrls: [ './layout.component.scss' ]
-} )
-export class LayoutProfesorComponent implements OnInit
-{
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutProfesorComponent implements OnInit {
   cursosSeleccionados: any
-  constructor () { }
+  constructor() { }
 
-  ngOnInit () { }
+  ngOnInit() { }
 }
