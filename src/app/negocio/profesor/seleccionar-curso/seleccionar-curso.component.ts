cf73351656d12276871da66929b55f8d6d3f2176
AngeloRP <angelorp93@gmail.com>
2019-05-30 23:33:50 -0500

    Cambios en el componente seleccionar curso agregando buscador en el select y componente tabla

--- src/app/negocio/profesor/seleccionar-curso/seleccionar-curso.component.ts
+++ src/app/negocio/profesor/seleccionar-curso/seleccionar-curso.component.ts
@@ -1,8 +1,14 @@
 import { Component, EventEmitter } from '@angular/core';
 import { Validators } from '@angular/forms';
+import { Escuela } from '@negocio/administrador/profesores/profesores.component';
 import { Formulario } from '@shared/formulario/formulario';
 
-import { Escuela } from '../../administrador/profesores/profesores.component';
+import { CursoSeleccionados } from './cursos-escogidos/cursos-escogidos.component';
+
+export interface RowSelect {
+  value: any,
+  viewValue: string
+}
 
 @Component({
   selector: 'app-seleccionar-curso',
@@ -10,51 +16,95 @@ import { Escuela } from '../../administrador/profesores/profesores.component';
   styleUrls: ['./seleccionar-curso.component.scss']
 })
 export class SeleccionarCursoComponent extends Formulario {
-  cursosSeleccionados = new EventEmitter<any>();
-  cursos = [
-    { value: 0, viewValue: 'Arquitectura de Software' },
-    { value: 1, viewValue: 'Diseño de Software' },
-    { value: 2, viewValue: 'Calidad de Software' },
-    { value: 3, viewValue: 'Base de Datos 1' },
-    { value: 4, viewValue: 'Base de Datos 2' },
-    { value: 5, viewValue: 'Teoria General de Sistemas' },
-    { value: 6, viewValue: 'Programación 1' },
-    { value: 7, viewValue: 'Programación 2' },
-    { value: 8, viewValue: 'Redes Neuronales' },
-    { value: 9, viewValue: 'Planeamiento de Recursos Empresariales' },
-    { value: 10, viewValue: 'Estructura de Datos 1' },
-    { value: 11, viewValue: 'Estructura de Datos 2' },
-    { value: 12, viewValue: 'Sistemas Inteligentes' },
-    { value: 13, viewValue: 'Inteligencia Artificial' }
-  ];
-  escuelas = [
-    { value: 0, viewValue: Escuela.Sistemas },
-    { value: 1, viewValue: Escuela.Software }
-  ];
+  cursosSeleccionadosEvento = new EventEmitter<any>();
+  cursosSeleccionados: Array<CursoSeleccionados>= []
+  cursos: Array<RowSelect>;
+  escuelas: Array<RowSelect>;
+  escuelasCursos: Array<RowSelect> = [];
+  searchValue = '';
+  valido = true;
+  temp = []
   constructor() {
     super([
-      { name: 'curso1', validators: [Validators.required] },
-      { name: 'curso2', validators: [Validators.required] },
-      { name: 'curso3', validators: [Validators.required] },
-      { name: 'curso4', validators: [Validators.required] },
-      { name: 'escuela1', validators: [Validators.required] },
-      { name: 'escuela2', validators: [Validators.required] },
-      { name: 'escuela3', validators: [Validators.required] },
-      { name: 'escuela4', validators: [Validators.required] }
+      { name: 'cursos', validators: [Validators.required] }
     ]);
-
+    this.escuelas = [
+      { value: 0, viewValue: Escuela.Sistemas },
+      { value: 1, viewValue: Escuela.Software }
+    ];
+    this.cursos = [
+      { value: 0, viewValue: 'Arquitectura de Software' },
+      { value: 1, viewValue: 'Diseño de Software' },
+      { value: 2, viewValue: 'Calidad de Software' },
+      { value: 3, viewValue: 'Base de Datos 1' },
+      { value: 4, viewValue: 'Base de Datos 2' },
+      { value: 5, viewValue: 'Teoria General de Sistemas' },
+      { value: 6, viewValue: 'Programación 1' },
+      { value: 7, viewValue: 'Programación 2' },
+      { value: 8, viewValue: 'Redes Neuronales' },
+      { value: 9, viewValue: 'Planeamiento de Recursos Empresariales' },
+      { value: 10, viewValue: 'Estructura de Datos 1' },
+      { value: 11, viewValue: 'Estructura de Datos 2' },
+      { value: 12, viewValue: 'Sistemas Inteligentes' },
+      { value: 13, viewValue: 'Inteligencia Artificial' }
+    ];
+    this.escuelas.forEach(
+      escuela => {
+        this.cursos.forEach(
+          curso => {
+            this.escuelasCursos.push(
+              {
+                value: `${escuela.value} - ${curso.value}`,
+                viewValue: `${escuela.viewValue} |  ${curso.viewValue}`
+              }
+            );
+          }
+        );
+      }
+    );
+    this.temp = this.escuelasCursos;
     this.formGroup.valueChanges.subscribe(
       result => {
-        console.log(result);
+        this.actualizarCursosSeleccionados(result.cursos);
       }
     );
     this.formGroup.statusChanges.subscribe(
       result => {
         console.log(result);
         if (result === 'VALID') {
-          this.cursosSeleccionados.emit(this.formGroup.value);
+          this.cursosSeleccionadosEvento.emit(this.formGroup.value);
         }
       }
     );
   }
+
+  buscar(value: string): void {
+    this.escuelasCursos = this.temp.filter(
+      item => item.viewValue.toUpperCase()
+        .includes(value.toUpperCase())
+    );
+  }
+
+  selectChange(event): void {
+    event.length > 3 ? this.valido = false : this.valido = true;
+  }
+
+  private actualizarCursosSeleccionados(identificadores:Array<string>): void {
+    console.log(identificadores);
+
+    const filtrado = this.escuelasCursos.filter(
+      escuelaCurso => {
+        console.log(escuelaCurso);
+
+        return identificadores.findIndex( identificador => escuelaCurso.value === identificador) !== -1}
+    );
+    this.cursosSeleccionados = []
+    filtrado.forEach( row => {
+      const separado = row.viewValue.split('|')
+      this.cursosSeleccionados.push({ escuela:separado[0], curso: separado[1]})
+    })
+
+
+  }
+
 }
